a1b95a9a48e2db733a6be6df8eb98817811cefe7
derrick <kang.derrick@gmail.com>
2018-01-07 17:28:59 +0900

    added circle view

---
+++ app/src/main/java/io/github/superbderrick/scoreboard/CircleView.java
@@ -0,0 +1,28 @@
+package io.github.superbderrick.scoreboard;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by derrick on 07/01/2018.
+ */
+
+public class CircleView extends View {
+    public CircleView(Context context) {
+        super(context);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
