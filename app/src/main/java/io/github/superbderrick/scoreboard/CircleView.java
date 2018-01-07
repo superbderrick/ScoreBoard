ad1830260f9e8a2e2826bd7c222ad83d495bcab5
derrick <kang.derrick@gmail.com>
2018-01-07 17:35:05 +0900

    working on circle view

--- app/src/main/java/io/github/superbderrick/scoreboard/CircleView.java
+++ app/src/main/java/io/github/superbderrick/scoreboard/CircleView.java
@@ -1,6 +1,8 @@
 package io.github.superbderrick.scoreboard;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -10,6 +12,8 @@ import android.view.View;
  */
 
 public class CircleView extends View {
+
+
     public CircleView(Context context) {
         super(context);
     }
@@ -24,5 +28,19 @@ public class CircleView extends View {
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init() {
+
+    }
+
+
+    //Draw APIS.
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        
     }
 }
