75adf3fb6390628af32d89e928a301e34dcd916b
derrick <kang.derrick@gmail.com>
2018-01-23 21:43:03 +0900

    working on circlview with customview style

--- app/src/main/java/io/github/superbderrick/scoreboard/CircleView.java
+++ app/src/main/java/io/github/superbderrick/scoreboard/CircleView.java
@@ -1,8 +1,11 @@
 package io.github.superbderrick.scoreboard;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -13,6 +16,15 @@ import android.view.View;
 
 public class CircleView extends View {
 
+    public int mCircleColor;
+
+    public int getmCircleColor() {
+        return mCircleColor;
+    }
+
+    public void setmCircleColor(int mCircleColor) {
+        this.mCircleColor = mCircleColor;
+    }
 
     public CircleView(Context context) {
         super(context);
@@ -20,12 +32,16 @@ public class CircleView extends View {
 
     public CircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mCircleColor = context.obtainStyledAttributes(attrs, R.styleable.NewAttr)
+                .getColor(R.styleable.NewAttr_circleColor, Color.WHITE);
+
     }
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
@@ -41,6 +57,9 @@ public class CircleView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        
+        paint.setColor(Color.BLUE);
+
+        canvas.drawCircle(this.getWidth()/2, 50, 20, paint);
+
     }
 }
