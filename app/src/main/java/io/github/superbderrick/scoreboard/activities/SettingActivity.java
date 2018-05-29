7155b60986312bb4f1181a448c42b8107d1bfd89
derrick <kang.derrick@gmail.com>
2018-05-29 20:39:02 +0900

    working on setting activity

--- app/src/main/java/io/github/superbderrick/scoreboard/activities/SettingActivity.java
+++ app/src/main/java/io/github/superbderrick/scoreboard/activities/SettingActivity.java
@@ -1,13 +1,12 @@
 package io.github.superbderrick.scoreboard.activities;
 
-import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.util.Log;
 
 import io.github.superbderrick.scoreboard.R;
@@ -24,20 +23,39 @@ public class SettingActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
-        
+
 
     }
 
-
-
-
     public static class MyPreferenceFragment extends PreferenceFragment
     {
+        ListPreference mGameTimePreference;
+        ListPreference mGameScorereference;
+        ListPreference mGameHandyPreference;
+        SharedPreferences mPrefs;
+        PreferenceScreen mKeywordScreen;
+
         @Override
         public void onCreate(final Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preference);
+            mPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+            //@string/gametime_key"
+            mGameTimePreference = (ListPreference)findPreference("gameTimekey");
+            mGameHandyPreference = (ListPreference)findPreference("handkey");
+            mGameScorereference = (ListPreference)findPreference("setscorekey");
+            mKeywordScreen = (PreferenceScreen)findPreference("keyword_screen");
+
+
+            mPrefs.registerOnSharedPreferenceChangeListener(prefListener);
         }
+        SharedPreferences.OnSharedPreferenceChangeListener prefListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                Log.d("derrick" , "s: " + s);
+            }
+        };
     }
 }
