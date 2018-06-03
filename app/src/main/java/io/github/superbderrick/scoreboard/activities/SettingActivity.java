1de52dcc029b0a3a010b35d755e4e10bce9d5df7
derrick <kang.derrick@gmail.com>
2018-06-03 14:23:22 +0900

    added email feature

--- app/src/main/java/io/github/superbderrick/scoreboard/activities/SettingActivity.java
+++ app/src/main/java/io/github/superbderrick/scoreboard/activities/SettingActivity.java
@@ -1,8 +1,11 @@
 package io.github.superbderrick.scoreboard.activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -29,6 +32,7 @@ public class SettingActivity extends PreferenceActivity {
     {
         ListPreference mGameScoreReference;
         ListPreference mGameHandyPreference;
+        Preference mEmailPreference;
         SharedPreferences mPrefs;
         PreferenceScreen mKeywordScreen;
 
@@ -43,6 +47,8 @@ public class SettingActivity extends PreferenceActivity {
             mGameHandyPreference = (ListPreference)findPreference("handkey");
             mGameScoreReference = (ListPreference)findPreference("setscorekey");
             mKeywordScreen = (PreferenceScreen)findPreference("keyword_screen");
+            mEmailPreference = (Preference)findPreference("contactDevKey");
+            mEmailPreference.setOnPreferenceClickListener(mPrefClickListener);
 
             String handSentence = Utils.getHandaySettingSentence(mGameHandyPreference.getValue());
             mGameHandyPreference.setSummary(handSentence);
@@ -61,6 +67,19 @@ public class SettingActivity extends PreferenceActivity {
                     String setSentence = Utils.getSetScoreSettingSentence(mGameScoreReference.getValue());
                     mGameScoreReference.setSummary(setSentence);
                 }
+            }
+        };
+
+        Preference.OnPreferenceClickListener mPrefClickListener = new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                emailIntent.setData(Uri.parse("mailto: kang.derrick@gmail.com"));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hi Derrick");
+                startActivity(Intent.createChooser(emailIntent, "Send feedback"));
+
+                return false;
             }
         };
     }
